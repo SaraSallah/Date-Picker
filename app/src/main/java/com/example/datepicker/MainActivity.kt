506ad4f0697209e84cf302aed37eaf5d7261a275
Your <93276124+SaraSallah@users.noreply.github.com>
2024-05-05 22:50:49 +0200

    add calender view

--- app/src/main/java/com/example/datepicker/MainActivity.kt
+++ app/src/main/java/com/example/datepicker/MainActivity.kt
@@ -5,6 +5,7 @@ import android.icu.util.Calendar
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,14 +20,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        textViewDate = binding.textViewDate
-        buttonPickDate = binding.buttonPickDate
-
-        buttonPickDate.setOnClickListener {
-            showDatePicker()
+//
+//        textViewDate = binding.textViewDate
+//        buttonPickDate = binding.buttonPickDate
+//
+//        buttonPickDate.setOnClickListener {
+//            showDatePicker()
+//        }
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val selectedDate = "$dayOfMonth/${month + 1}/$year"
+            Toast.makeText(this, "Selected date: $selectedDate", Toast.LENGTH_SHORT).show()
         }
     }
+
     private fun showDatePicker() {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
